b6b8dfcd9a92071b0b65f47c814ae632f7d8b33c
chrisweb <chris963@gmail.com>
2023-06-21 22:53:31 +0200

    new build

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -1,3 +1,3 @@
 type Root = import('mdast').Root;
-export declare const remarkTocMdx: () => (ast: Root) => Promise<void>;
+export declare const remarkTocMdx: () => (ast: Root) => Promise<import("mdast").Root | undefined>;
 export {};
