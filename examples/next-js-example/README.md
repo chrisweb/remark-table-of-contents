e1130db5b1ebae10e5848e0f5826018d41502bfb
chrisweb <chris963@gmail.com>
2023-06-26 23:15:08 +0200

    added next-js example

---
+++ examples/next-js-example/README.md
@@ -0,0 +1,13 @@
+# next-js-example
+
+## install the dependencies
+
+use the following command to install the dependencies (node_modules):
+
+`npm run dev`
+
+## dev (local) server
+
+to start the dev server, use your terminal and then go into the root of this example, then run the following command:
+
+`npm run dev`
---
+++ examples/next-js-example/next.config.mjs
@@ -0,0 +1,27 @@
+import WithMDX from '@next/mdx'
+import { remarkTableOfContents } from '../../dist/index.js'
+
+const nextConfig = (/*phase*/) => {
+
+    const withMDX = WithMDX({
+        extension: /\.mdx?$/,
+        options: {
+            remarkPlugins: [[remarkTableOfContents, { tight: true }]],
+            rehypePlugins: [],
+        },
+    })
+
+    /** @type {import('next').NextConfig} */
+    const nextConfig = {
+        experimental: {
+            appDir: true,
+            mdxRs: false,
+        },
+        pageExtensions: ['ts', 'tsx', 'js', 'jsx', 'md', 'mdx'],
+    }
+
+    return withMDX(nextConfig)
+
+}
+
+export default nextConfig
