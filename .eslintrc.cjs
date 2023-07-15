f8a833c0ad102805d969d5110fdec006b7ee1bcf
chrisweb <chris963@gmail.com>
2023-07-15 21:42:51 +0200

    eslint update, added root

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,29 +1,30 @@
 module.exports = {
-    'env': {
-        'browser': true,
-        'es2021': true,
-        'node': true
+    env: {
+        browser: true,
+        es2021: true,
+        node: true
     },
-    'extends': [
+    extends: [
         'eslint:recommended',
         'plugin:@typescript-eslint/recommended'
     ],
-    'overrides': [
+    overrides: [
     ],
-    'parser': '@typescript-eslint/parser',
-    'parserOptions': {
-        'ecmaVersion': 'latest',
-        'sourceType': 'module'
+    parser: '@typescript-eslint/parser',
+    parserOptions: {
+        ecmaVersion: 'latest',
+        sourceType: 'module'
     },
-    'plugins': [
+    plugins: [
         '@typescript-eslint'
     ],
-    'rules': {
-        'quotes': [
+    root: true,
+    rules: {
+        quotes: [
             'error',
             'single',
         ],
-        'semi': [
+        semi: [
             'error',
             'never',
         ],
